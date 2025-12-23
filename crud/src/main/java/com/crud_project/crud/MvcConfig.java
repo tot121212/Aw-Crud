bff7d01ee91f074b7cf2700eed5babf0ed9c9e20
Totsnuk <evanmaywald2@gmail.com>
2025-12-23 17:36:37 -0600

    fix structure

--- crud/src/main/java/com/crud_project/crud/MvcConfig.java
+++ crud/src/main/java/com/crud_project/crud/MvcConfig.java
@@ -12,10 +12,7 @@ public class MvcConfig implements WebMvcConfigurer {
 		registry.addViewController("/").setViewName("home");
 		registry.addViewController("/login").setViewName("login");
         registry.addViewController("/register").setViewName("register");
-        registry.addViewController("/create").setViewName("create");
-        registry.addViewController("/read").setViewName("read");
-        registry.addViewController("/update").setViewName("update");
-        registry.addViewController("/delete").setViewName("delete");
+        registry.addViewController("/crud").setViewName("crud");
 	}
 
 }
