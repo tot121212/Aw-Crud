5616a0759d7fa2f71cf16cad9e168693519e70ca
Totsnuk <evanmaywald2@gmail.com>
2025-12-23 14:09:46 -0600

    removed hello.html, added general css alignment and gap, added basic html pages, added html pages to mvcconfig

---
+++ crud/src/main/java/com/crud_project/crud/MvcConfig.java
@@ -0,0 +1,21 @@
+package com.crud_project.crud;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/").setViewName("home");
+		registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/register").setViewName("register");
+        registry.addViewController("/create").setViewName("create");
+        registry.addViewController("/read").setViewName("read");
+        registry.addViewController("/update").setViewName("update");
+        registry.addViewController("/delete").setViewName("delete");
+	}
+
+}
