20e55cce039cb5715996e970ba064905fdf4fdab
Totsnuk <evanmaywald2@gmail.com>
2026-01-21 16:26:44 -0600

    added rate limiting

---
+++ crud/src/main/java/com/crud_project/crud/RateLimitingFilter.java
@@ -0,0 +1,40 @@
+package com.crud_project.crud;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class RateLimitingFilter implements Filter {
+    private static final int MAX_REQUESTS_PER_MIN = 50;
+    private final Map<String, AtomicInteger> requestCounts = new ConcurrentHashMap<>();
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException{
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        String clientIp = httpRequest.getRemoteAddr();
+        requestCounts.putIfAbsent(clientIp, new AtomicInteger(0));
+        int currentCount = requestCounts.get(clientIp).incrementAndGet();
+
+        if (currentCount > MAX_REQUESTS_PER_MIN) {
+            httpResponse.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+            httpResponse.getWriter().write("Too many requests. Please try again later.");
+            return;
+        }
+        chain.doFilter(request, response);
+    }
+}
