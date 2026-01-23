3f49d54b2eef9719487593bea7f85766b19be55e
Totsnuk <evanmaywald2@gmail.com>
2026-01-23 13:08:43 -0600

    security fix: UserProjection and UserProjectionImpl should not show ID

--- crud/src/main/java/com/crud_project/crud/entity/UserProjectionImpl.java
+++ crud/src/main/java/com/crud_project/crud/entity/UserProjectionImpl.java
@@ -4,23 +4,19 @@ import java.io.Serializable;
 
 import com.crud_project.crud.repository.UserProjection;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Value;
 
 // This is for serialization if needed
-@Getter
+@Value
 @Builder
-@AllArgsConstructor
 public class UserProjectionImpl implements UserProjection, Serializable {
-    private final int id;
     private final String userName;
     private final int awCrudsPerformed;
     private final boolean dead;
 
     public static UserProjectionImpl from(UserProjection projection) {
         return UserProjectionImpl.builder()
-            .id(projection.getId())
             .userName(projection.getUserName())
             .awCrudsPerformed(projection.getAwCrudsPerformed())
             .dead(projection.isDead())
--- crud/src/main/java/com/crud_project/crud/repository/UserProjection.java
+++ crud/src/main/java/com/crud_project/crud/repository/UserProjection.java
@@ -1,7 +1,6 @@
 package com.crud_project.crud.repository;
 
 public interface UserProjection {
-    int getId();
     String getUserName();
     int getAwCrudsPerformed();
     boolean isDead();
