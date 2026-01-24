37901cf5c7a72f8953587b7ab95fd812d034c578
Totsnuk <evanmaywald2@gmail.com>
2026-01-24 00:06:47 -0600

    fix: remove old sessionkey

--- crud/src/main/java/com/crud_project/crud/controller/SessionKeys.java
+++ crud/src/main/java/com/crud_project/crud/controller/SessionKeys.java
@@ -3,6 +3,5 @@ package com.crud_project.crud.controller;
 public final class SessionKeys {
     private SessionKeys() {}
 
-    public static final String CUR_USER_NAME = "currentUser_name";
     public static final String CUR_USER_PAGE_STATE = "currentUser_pageState";
 }
