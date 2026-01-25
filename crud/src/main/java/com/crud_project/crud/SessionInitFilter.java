ed3c1981dc76c89e16bed0ad48b546fabf297d45
Totsnuk <evanmaywald2@gmail.com>
2026-01-24 19:40:18 -0600

    add: SessionInitFilter which initializes session variables on first request
    remove:
    old implementation within WebSecurityConfig of initializing session variables
    
    fix:
    add logout HttpSecurity instead of default for proper redirect to login page
    
    fix:
    add /error to requestMatchers

---
+++ crud/src/main/java/com/crud_project/crud/SessionInitFilter.java
@@ -0,0 +1,44 @@
+package com.crud_project.crud;
+
+import java.io.IOException;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
+
+import com.crud_project.crud.controller.SessionKeys;
+import com.crud_project.crud.entity.PageState;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@Component
+public class SessionInitFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        HttpSession session = request.getSession(false); // don't create if it doesn't exist
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (session != null && auth != null && auth.isAuthenticated()) {
+            Object mutex = WebUtils.getSessionMutex(session);
+
+            synchronized (mutex) {
+                if (session.getAttribute(SessionKeys.CUR_USER_PAGE_STATE) == null) {
+                    session.setAttribute(SessionKeys.CUR_USER_PAGE_STATE, new PageState());
+                }
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
--- crud/src/main/java/com/crud_project/crud/WebSecurityConfig.java
+++ crud/src/main/java/com/crud_project/crud/WebSecurityConfig.java
@@ -1,69 +1,43 @@
 package com.crud_project.crud;
 
-import java.io.IOException;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
-import com.crud_project.crud.controller.SessionKeys;
-import com.crud_project.crud.entity.PageState;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
+
     private final UserDetailsServiceImpl userDetailsService;
-
-    private void setupInitialState(Authentication authentication, HttpSession session){
-        session.setAttribute(SessionKeys.CUR_USER_NAME, authentication.getName());
-        session.setAttribute(SessionKeys.CUR_USER_PAGE_STATE, PageState.builder().build());
-    }
-
-    @Bean
-    AuthenticationSuccessHandler loginSuccessHandler() {
-        return new SimpleUrlAuthenticationSuccessHandler() {
-            @Override
-            public void onAuthenticationSuccess(
-                HttpServletRequest request, 
-                HttpServletResponse response, 
-                Authentication authentication) throws IOException 
-            {
-                HttpSession session = request.getSession();
-                setupInitialState(authentication, session);
-                getRedirectStrategy().sendRedirect(request, response, "/crud");
-            }
-        };
-    }
 
     @Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
+            .userDetailsService(userDetailsService)
 			.authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/", "/home", "/auth/register", "/auth/login", "/css/**", "/data/**", "/js/**", "/svg/**")
+                .requestMatchers("/error", "/", "/home", "/auth/register", "/auth/login", "/css/**", "/data/**", "/js/**", "/svg/**", "/.well-known/**")
                 .permitAll()
                 .anyRequest().authenticated())
 			.formLogin((form) -> form
 				.loginPage("/auth/login")
-                .successHandler(loginSuccessHandler())
                 .failureUrl("/auth/login?error=true")
 				.permitAll()
 			)
-			.logout(LogoutConfigurer::permitAll)
-            .userDetailsService(userDetailsService);
+			.logout(logout -> logout
+                .logoutUrl("/auth/logout")
+                .logoutSuccessUrl("/auth/login?logout=true")
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .permitAll()
+            )
+        ;
 		return http.build();
 	}
 
