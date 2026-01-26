9c9d5c284da6762b93761e6a585af5e30d002bb9
Totsnuk <evanmaywald2@gmail.com>
2026-01-26 11:55:32 -0600

    chore: POM - add webmvc, autoconfigure, and web

--- crud/src/main/java/com/crud_project/crud/CrudApplication.java
+++ crud/src/main/java/com/crud_project/crud/CrudApplication.java
@@ -2,7 +2,9 @@ package com.crud_project.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @SpringBootApplication
 public class CrudApplication {
 
