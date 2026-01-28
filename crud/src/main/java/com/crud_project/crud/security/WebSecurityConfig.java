ac72d29d626dc49ebd2fc599727d9552e06f2b0f
tot121212 <git@github.com:tot121212>
2026-01-28 17:25:17 -0600

    chore: reformat WebSecurityConfig

--- crud/src/main/java/com/crud_project/crud/security/WebSecurityConfig.java
+++ crud/src/main/java/com/crud_project/crud/security/WebSecurityConfig.java
@@ -18,30 +18,34 @@ public class WebSecurityConfig {
     private final UserDetailsServiceImpl userDetailsService;
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .userDetailsService(userDetailsService)
-                .authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/error", "/", "/home", "/auth/register", "/auth/login", "/css/**", "/data/**", "/js/**", "/svg/**", "/.well-known/**")
-                .permitAll()
-                .anyRequest().authenticated())
-                .formLogin((form) -> form
-                .loginPage("/auth/login")
-                .failureUrl("/auth/login?error=true")
-                .permitAll()
+                .authorizeHttpRequests(
+                        (requests) -> requests
+                                .requestMatchers("/error", "/", "/home", "/auth/register", "/auth/login", "/css/**", "/data/**", "/js/**", "/svg/**", "/.well-known/**")
+                                .permitAll()
+                                .anyRequest().authenticated()
                 )
-                .logout(logout -> logout
-                .logoutUrl("/auth/logout")
-                .logoutSuccessUrl("/auth/login?logout=true")
-                .invalidateHttpSession(true)
-                .clearAuthentication(true)
-                .permitAll()
+                .formLogin(
+                        (form) -> form
+                                .loginPage("/auth/login")
+                                .failureUrl("/auth/login?error=true")
+                                .permitAll()
+                )
+                .logout(
+                        logout -> logout
+                                .logoutUrl("/auth/logout")
+                                .logoutSuccessUrl("/auth/login?logout=true")
+                                .invalidateHttpSession(true)
+                                .clearAuthentication(true)
+                                .permitAll()
                 );
         return http.build();
     }
 
     @Bean
-    PasswordEncoder passwordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
